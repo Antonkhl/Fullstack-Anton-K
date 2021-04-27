f8996d2b0618df357b5181eaffc84c2b8c771d06
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2021-04-27 10:53:30 +0200

    Commit 3
    Arbetade mest med views och html, la till allt html som jag ska kommer att behöva till mina sidor.
    
    Blev klar med html för index.ejs, index2.ejs, logIn.ejs, register.ejs. Dessutom i backenden la jag till get requests till alla sidor som jag har.

--- app.js
+++ app.js
@@ -15,6 +15,39 @@ app.get('', (req,res) => {
     res.render('index')
 })
 
+app.get('/index2', (req,res) => {
+    res.render('index2')
+})
+
+app.get('/aboutus', (req,res) => {
+    res.render('aboutus')
+})
+
+app.get('/createcategory', (req,res) => {
+    res.render('createcategory')
+})
+
+app.get('/createpost', (req,res) => {
+    res.render('createpost')
+})
+
+app.get('/logIn', (req,res) => {
+    res.render('logIn')
+})
+
+app.get('/logIn/loginsucessful', (req,res) => {
+    res.render('loginsucessful')
+})
+
+app.get('/register', (req,res) => {
+    res.render('register')
+})
+
+app.get('/register/registersucessful', (req,res) => {
+    res.render('registersucessful')
+})
+
+
 
 
 
